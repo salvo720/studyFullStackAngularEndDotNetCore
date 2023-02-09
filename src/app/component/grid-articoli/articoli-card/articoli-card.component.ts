c2c9d169e0b19bae7f16301dd1f91c70a8297312
salvo720 <salvocelano91@gmail.com>
2023-02-09 18:26:12 +0100

    compleato video 39 , inizio 40 introduzione ai web services , aggiunta event emitter su articoli-cad per comunicare con il componente padre

--- src/app/component/grid-articoli/articoli-card/articoli-card.component.ts
+++ src/app/component/grid-articoli/articoli-card/articoli-card.component.ts
@@ -1,5 +1,5 @@
 import { Iarticoli } from './../../../model/interface/iarticoli';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-articoli-card',
@@ -9,21 +9,30 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ArticoliCardComponent implements OnInit {
   @Input() articolo: Iarticoli
 
+  @Output() edit: EventEmitter<any>
+  @Output() delete: EventEmitter<any>
+
   constructor() {
     this.articolo = {
-      codart: '', 
-      descrizione: '', 
-      um: '', 
-      pzcart: 0, 
-      peso: 0, 
-      prezzo: 0, 
-      active: true, 
-      data: new Date(), 
+      codart: '',
+      descrizione: '',
+      um: '',
+      pzcart: 0,
+      peso: 0,
+      prezzo: 0,
+      active: true,
+      data: new Date(),
       imageUrl: ''
     }
+
+    this.edit = new EventEmitter();
+    this.delete = new EventEmitter();
   }
 
   ngOnInit(): void {
   }
+
+  articoloEdit() :void { this.edit.emit({"$event":event,"codart":this.articolo.codart}) }
+  articoloDelete() :void { this.delete.emit({"$event":event,"codart":this.articolo.codart}) }
 
 }
--- src/app/component/grid-articoli/grid-articoli.component.ts
+++ src/app/component/grid-articoli/grid-articoli.component.ts
@@ -1,6 +1,7 @@
 import { Iarticoli } from './../../model/interface/iarticoli';
 import { ArticoliService } from './../../service/articoli/articoli.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-grid-articoli',
@@ -17,4 +18,23 @@ export class GridArticoliComponent implements OnInit {
     console.log('articoli$ : ',this.articoli$)
   }
 
+  handleEdit(data:EventData){
+    let { $event , codart } =data;
+    console.log("event :" , $event , "cliccato tasto modifica del codice : " , codart)
+  }
+
+  handleDelete(data:EventData){
+    console.log(data)
+      const { $event, codart } = data;
+      console.log("event :" , $event , "cliccato tasto modifica del codice : " , codart)
+      this.articoli$.splice(this.articoli$.findIndex(x=> x.codart === codart),1)
+      console.log("event :" , $event , "cliccato tasto modifica del codice : " , codart)
+
+  }
+}
+
+// custom type per edit e delete typescript
+type EventData = {
+  $event : Event,
+  codart : string
 }
