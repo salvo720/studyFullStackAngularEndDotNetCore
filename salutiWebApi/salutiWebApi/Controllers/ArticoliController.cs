3ae95e96dc7379c725bd832a1df7cca254ded853
salvo720 <salvocelano91@gmail.com>
2023-02-17 14:11:32 +0100

    fermo al video 82 - Creazione del metodo di Ricerca del codice articolo , al minuto 00:00 , i dati restituiti dal metodo SelArticoliByDescrizione() adesso ritornano il tipo articoliDto , nel quale possiamo non mostrare dei campi alla view , articoliDto viene anche usato per eliminare i valori null ( fanno eccezzione i campi pzcart , pesonetto e dataCreazione ) , metodo SelArticoliByDescrizione() reso asincrono , aggiungendo async e await nel metodo chiamato dal repository SelArticoliByDescrizione()  , cambiando il tipo di ritorno da IActionResult<Articoli>  in Task<IActionResult<Articoli>> , modificato anche l'interfaccia IArticoliRepository e stato reso asincrono , e di conseguenza anche il repository ArticoliRepository e stato reso asyncrono , rinominata articolyRepository>ArticoliRepository .

--- salutiWebApi/salutiWebApi/Controllers/ArticoliController.cs
+++ salutiWebApi/salutiWebApi/Controllers/ArticoliController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using salutiWebApi.Dtos;
 using salutiWebApi.Models;
 using salutiWebApi.Service;
 
@@ -20,15 +21,47 @@ namespace salutiWebApi.Controllers
 
     [HttpGet("cerca/descrizione/{filter}")]
     [ProducesResponseType(400)]
-    [ProducesResponseType(200 , Type = typeof(IEnumerable<Articoli>))]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<ArticoliDto>))]
 
-    public IActionResult GetArticoliByDesc(string filter)
+    public async Task<IActionResult> GetArticoliByDesc(string filter)
     {
-      // IActionResult indica che otteremo degli articoli in formato json e anche uno stato 
-      // che ritornera una bad request se qualcosa non e andata per il verso giusto 
-      var articoli = _articoliRepository.SelArticoliByDescrizione(filter);
+      // IActionResult indica che otteremo degli articoli in formato json e anche uno stato
+      // che ritornera una bad request se qualcosa non e andata per il verso giusto
+      var articoliDto = new List<ArticoliDto>();
 
-      return Ok(articoli);
+      var articoli = await _articoliRepository.SelArticoliByDescrizione(filter);
+
+      // guard codiction
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      if (articoli.Count() == 0)
+      {
+        // ritrno errore 404
+        return NotFound(string.Format("Non e stato trovato alcun articolo con il filtro '{0}'", filter));
+      }
+
+      // code
+
+      foreach (var articolo in articoli)
+      {
+        articoliDto.Add(new ArticoliDto
+        {
+          CodArt = articolo.CodArt,
+          Descrizione = articolo.Descrizione,
+          Um = articolo.Um,
+          IdStatoArt = articolo.IdStatoArt,
+          PzCart = articolo.PzCart,
+          PesoNetto = articolo.PesoNetto,
+          DataCreazione = articolo.DataCreazione,
+        });
+      }
+
+
+      return Ok(articoliDto);
     }
   }
 }
